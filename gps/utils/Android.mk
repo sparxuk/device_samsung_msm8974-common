8ec873bfd7f7e94e48f0bc11455524ce21ea95c4
alliancesparx <alliancesparx@gmail.com>
2019-11-02 16:08:08 +0000

    msm8974: GPS: Add -Wno-missing-field-initializers to utils LOCAL_CFLAGS

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -26,6 +26,8 @@ LOCAL_SRC_FILES += \
 LOCAL_CFLAGS += \
      -fno-short-enums \
      -D_ANDROID_
+ 
+LOCAL_CFLAGS += -Wno-missing-field-initializers
 
 ifeq ($(TARGET_BUILD_VARIANT),user)
    LOCAL_CFLAGS += -DTARGET_BUILD_VARIANT_USER
